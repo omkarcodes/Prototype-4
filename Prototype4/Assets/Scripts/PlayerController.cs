4c920dc195b0fdc777ff42b5498fafcb2bfd98e0
Omkar Gaikwad <80238904+omkarcodes@users.noreply.github.com>
2023-02-28 01:40:51 +0530

    added the playercontrol
    New Functionality:
    Camera rotates around the island based on horizontal input
    Player rolls in direction of camera based on vertical input
    New Concepts and Skills:
    Texture Wraps
    Camera as child object
    Global vs Local coordinates
    Get direction of other object

---
+++ Prototype4/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    float speed = 10;
+    Rigidbody playerRb;
+    GameObject focalPoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        focalPoint = GameObject.Find("FocalPoint");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float verticalInput = Input.GetAxis("Vertical");
+        playerRb.AddForce(focalPoint.transform.forward * speed * verticalInput);
+    }
+}
