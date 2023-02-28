c299cf12879753ffc23ab06ffedf221e8e574e94
Omkar Gaikwad <80238904+omkarcodes@users.noreply.github.com>
2023-02-28 08:08:40 +0530

    added the enemies into the game
    New Functionality:
    Enemy spawns at random location on the island
    Enemy follows the player around
    Spheres bounce off of each other
    New Concepts and Skills:
    Physics Materials
    Defining vectors in 3D space
    Normalizing values
    Methods with return values

---
+++ Prototype4/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+    float spawnRange = 9;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+       
+        InvokeRepeating("SpawnEnemy", 1.5f, 2.5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void SpawnEnemy()
+    {
+        Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+    }
+
+    Vector3 GenerateSpawnPosition()
+    {
+        float spawnPosX = Random.Range(-spawnRange, spawnRange); 
+        float spawnPosY = Random.Range(-spawnRange, spawnRange);
+
+        Vector3 spawnPos = new Vector3(spawnPosX, 0, spawnPosY);
+        return spawnPos;
+    }
+}
