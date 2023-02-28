54a114f4ddcdba9d02a84dd82aea2f572605351d
Omkar Gaikwad <80238904+omkarcodes@users.noreply.github.com>
2023-02-28 15:56:09 +0530

    added the powerup feature and the countdown
    New Functionality:
    When the player collects a powerup, a visual indicator appears
    When the player collides with an enemy while they have the powerup, the enemy goes flying
    After a certain amount of time, the powerup ability and indicator disappear
    New Concepts and Skills:
    Debug concatenation
    Local component variables
    IEnumerators and WaitForSeconds()
    Coroutines
    SetActive(true/false)

--- Prototype4/Assets/Scripts/SpawnManager.cs
+++ Prototype4/Assets/Scripts/SpawnManager.cs
@@ -5,13 +5,14 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject enemyPrefab;
+    public GameObject powerupPrefab;
     float spawnRange = 9;
     
     // Start is called before the first frame update
     void Start()
     {
-       
-       
+
+        Instantiate(powerupPrefab,new Vector3(0,0,6), powerupPrefab.transform.rotation);
         InvokeRepeating("SpawnEnemy", 1.5f, 2.5f);
     }
 
