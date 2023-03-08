a62a4648af464d7f8066de0f71b499963b92a611
Omkar Gaikwad <80238904+omkarcodes@users.noreply.github.com>
2023-03-09 00:57:49 +0530

    Added player and made the camera move

---
+++ Prototype4/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    float speed = 10f;
+    Rigidbody playerRb;
+    GameObject focalPoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+
+        focalPoint = GameObject.Find("FocalPoint");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float verticalInput = Input.GetAxis("Vertical");
+
+        playerRb.AddForce(focalPoint.transform.forward * speed * verticalInput);
+    }
+}
---
+++ Prototype4/Assets/Scripts/RotateCamera.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCamera : MonoBehaviour
+{
+    float Rotationspeed = 50f;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+        transform.Rotate(Vector3.up * Rotationspeed * horizontalInput * Time.deltaTime);
+    }
+}
